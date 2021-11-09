dcaf220763dc5246b8ea2a5bb1100e12806b9430
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2021-11-09 15:16:00 +0100

    Script to retreive occurencies of a certain symbol in a specific day in tweets.Not working due to library twitterscraper error

--- scrapers/twitter_web_scraper/twitter_scraper_script.py
+++ scrapers/twitter_web_scraper/twitter_scraper_script.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+from twitterscraper import query_tweets
+import datetime
+#test
+print(get_symbol_occurencies_in_day("Shiba OR Shiba inu OR SHIB", datetime.date.today()))
+
+def get_symbol_occurencies_in_day(symbol, date):
+    occurencies = 0
+    
+    for tweet in query_tweets(str(symbol), limit=None, begindate=date, enddate=date, poolsize=20):
+        occurencies = occurencies + 1
+
+    return occurencies
