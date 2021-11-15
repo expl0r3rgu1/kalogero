5e89e9cde9b13fd6ace3e40309ae35662aa7318e
LUCA BABBONI <luca.babboni2@studio.unibo.it>
2021-11-15 22:03:54 +0100

    Initialized the api and made a simple text of working

--- scrapers/reddit_web_scraper/reddit_scraper_script.py
+++ scrapers/reddit_web_scraper/reddit_scraper_script.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
+
+from typing import Counter
 import praw 
 import json
 
-with open("scrapers/reddit_web_scraper/config.json", "r") as file: 
+#open a file called config.json.
+with open("config.json", "r") as file: 
     config = json.load(file)
 
 id = config["client_id"]
@@ -10,6 +14,7 @@ secret = config["secret"]
 reddit_username = config["reddit_username"]
 reddit_password = config["reddit_password"]
 
+#configure the bot
 reddit = praw.Reddit(
     client_id= id ,
     client_secret = secret,
@@ -18,16 +23,28 @@ reddit = praw.Reddit(
     username = reddit_username,
 )
 
+#if the log in is correct will print the reddit username
+print("\n")
 print(reddit.user.me())
+print("\n")
 
-subreddit = reddit.subreddit("wallstreetbets")
+#subreddit = reddit.subreddit("wallstreetbets")
 
-for submission in reddit.subreddit("wallstreetbets").new(limit=5):
-    print("Title:", submission.title)
-    print("Text:", submission.selftext)
-    print("Score:", submission.score)
-    print("-----------------------\n")
+new_wallstreetbest = reddit.subreddit("wallstreetbets").hot(limit = 15)
 
+counter = 0
+
+for submission in new_wallstreetbest:
+    if not submission.stickied:
+        print(counter)
+        print(") \n")
+        print("Title:", submission.title)
+        if (submission.selftext ):
+            print("Text:", submission.selftext)
+        print("Score:", submission.score)
+        print("-----------------------\n")
+        counter = counter + 1
+        
 
 
 pass
