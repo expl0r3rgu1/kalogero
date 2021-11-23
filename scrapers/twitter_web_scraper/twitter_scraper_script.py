f2f1fcc1b6894459f0b2393bdb11255cea5dbac5
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2021-11-23 09:28:47 +0100

    Getting Pandas data frame from json file

--- scrapers/twitter_web_scraper/twitter_scraper_script.py
+++ scrapers/twitter_web_scraper/twitter_scraper_script.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import os
 import datetime
+import pandas as pd
 
 def get_symbol_occurrencies_in_day(symbol, date):
     next_day = date + datetime.timedelta(days=1)
     os.system("snscrape --jsonl --progress --since " + date.strftime("%Y") + "-" + date.strftime("%m") + "-" + date.strftime("%d") + " twitter-search \'" + symbol + " " + "until:" + date.strftime("%Y") + "-" + date.strftime("%m") + "-" + next_day.strftime("%d") + "\' > text-query-tweets.json")
+
+    tweets_df = pd.read_json('text-query-tweets.json', lines=True)
 
 #test
 get_symbol_occurrencies_in_day("SHIB", datetime.date.today())
