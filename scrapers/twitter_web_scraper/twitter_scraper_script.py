50a23138a1da452d94e02bbabaa27fef293f034e
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2021-11-23 08:41:18 +0100

    Added snscrape syscall that generates a json file with the query tweet objects

--- scrapers/twitter_web_scraper/twitter_scraper_script.py
+++ scrapers/twitter_web_scraper/twitter_scraper_script.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+import os
 
+#test
+print(get_symbol_occurrencies_in_day("SHIB", datetime.date.today()))
+
+def get_symbol_occurrencies_in_day(symbol, date):
+    next_day = date.datetime.now() + date.timedelta(days=1)
+    os.system("snscrape --json --since " + date.strftime("%Y") + "-" + date.strftime("%m") + "-" + date.strftime("%d") + " twitter-search \""+ symbol + "until:" + date.strftime("%Y") + "-" + date.strftime("%m") + "-" + next_day.strftime("%d") + "\" > text-query-tweets.json")
