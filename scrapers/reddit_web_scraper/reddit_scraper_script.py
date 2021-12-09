e9af72f7140f75b6313a2d9f22ee10bf6e01784a
Luca Babboni <luca.babboni01@gmail.com>
2021-12-09 21:30:00 +0100

    added some experiments for the date check

--- scrapers/reddit_web_scraper/reddit_scraper_script.py
+++ scrapers/reddit_web_scraper/reddit_scraper_script.py
@@ -4,8 +4,9 @@ from typing import Counter
 import praw
 import json
 from datetime import datetime
+from datetime import date
+import time
 import os
-
 from praw.models import subreddits
 from prawcore.exceptions import OAuthException
 
@@ -40,7 +41,9 @@ def openFileConfig():
             
 def checkConfigOk(config):
     if config["client_id"] == "" or config["secret"] == "" or config["reddit_username"] == "" or config["reddit_password"] == "" or config["subreddits"] == [""]:
-        print("ERROR: controll the file config.json and instert the credential \n you can find the file in the path: " + dir)
+        print("ERROR: controll the file config.json and instert the credential \n you can find the file in the path: ")
+        print(dir)
+
         exit()
 
 def checkLogin(reddit): 
@@ -56,7 +59,20 @@ def printOccurences(symbols, symbolsOccurrence):
         print(idx[1],":", symbolsOccurrence[idx[0]])
 
 
+
 #--------------------program starts here---------------------
+
+#--------scarabocchi per trovare la data------------------ 
+s = "01/12/2011"
+print(time.mktime(datetime.strptime(s, "%d/%m/%Y").timetuple()))
+
+#1322697600.0
+
+firstDate = date(2001,5,4)
+print(firstDate)
+#----------------------------------------------------------
+
+
 
 # open a file called config.json.
 config = openFileConfig()
@@ -89,16 +105,18 @@ counter = 0
 for subreddit in subreddits:
     print("\n \n \n analizing subreddit:" + subreddit)
 
-    for submission in reddit.subreddit(subreddit).new():
-        if not submission.stickied:
-            firstDate = datetime.utcfromtimestamp(submission.created_utc)
-            break
+    #for submission in reddit.subreddit(subreddit).new():
+    #    if not submission.stickied:
+    #        firstDate = datetime.utcfromtimestamp(submission.created_utc)
+    #        break
+
+    #insert here the code for find the first submission of the chosen day
+
 
     for submission in reddit.subreddit(subreddit).new():
         submissionDate = datetime.utcfromtimestamp(submission.created_utc)
         if sameDay(firstDate, submissionDate):
             if not submission.stickied:
-                counter = counter + 1
                 #textcheck contains the title and text of the post in lower case
                 textocheck = (submission.title + submission.selftext).lower()
                 #count occurrences
